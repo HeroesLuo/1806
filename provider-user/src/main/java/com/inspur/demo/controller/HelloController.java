4dcc5fd5978a7e4fa166e040b14c6d68ab0d222e
HeroesLuo <HeroesLuo>
2018-12-09 12:37:58 +0800

    1806coding

---
+++ provider-user/src/main/java/com/inspur/demo/controller/HelloController.java
@@ -0,0 +1,15 @@
+package com.inspur.demo.controller;
+
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+	@RequestMapping("/hello/{name}")
+	public String hello(@PathVariable("name") String name) {
+		
+		return "hello SpringCloud 1 "+name;
+	}
+
+}
